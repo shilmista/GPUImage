7b7e65765b70be4d0991678bd31fdc04bfcd921d
Tony Cheng <shilmista@gmail.com>
2015-06-18 16:23:26 -0400

    Testing GPUImageAlphaBlendFilter change

--- framework/Source/GPUImageAlphaBlendFilter.m
+++ framework/Source/GPUImageAlphaBlendFilter.m
@@ -16,7 +16,9 @@ NSString *const kGPUImageAlphaBlendFragmentShaderString = SHADER_STRING
 	 lowp vec4 textureColor = texture2D(inputImageTexture, textureCoordinate);
 	 lowp vec4 textureColor2 = texture2D(inputImageTexture2, textureCoordinate2);
 	 
-	 gl_FragColor = vec4(mix(textureColor.rgb, textureColor2.rgb, textureColor2.a * mixturePercent), textureColor.a);
+//	 gl_FragColor = vec4(mix(textureColor.rgb, textureColor2.rgb, textureColor2.a * mixturePercent), textureColor.a);
+     lowp float alphaDivisor = textureColor2.a + step(textureColor2.a, 0.0); // Protect against a divide-by-zero blacking out things in the output
+     gl_FragColor = vec4(mix(textureColor.rgb, textureColor2.rgb / alphaDivisor, mixturePercent * textureColor2.a), textureColor.a);
  }
 );
 #else
